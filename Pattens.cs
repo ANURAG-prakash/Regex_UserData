1c9cefbb4329ca644e97560549ce6472bc9af7c2
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-24 20:19:39 +0530

    Anurag Prakash :Has added UC1

---
+++ Pattens.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Pattens
+    {
+        public static string Regex_Name = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ToCheckName(string name)
+        {
+            return Regex.IsMatch(name, Regex_Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattens name = new Pattens();
+            Console.WriteLine("Enter Name To Enter");
+            string Name = Console.ReadLine();
+            if (name.ToCheckName(Name) == true)
+                Console.WriteLine("Valid Input");
+            else
+                Console.WriteLine("InValid Input");
+
+        }
+    }
+}
