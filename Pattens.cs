3cd70e543941492f2ea4afcec14d8c395b518312
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-29 00:44:08 +0530

    Anurag Prakash has added lamda Expression

---
+++ Pattens.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Pattens
+    {
+        public static string Regex_Name = "^[A-Z]{1}[a-z]{2,}$";
+        public static string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
+        public static string Regex_Email = "^[a-z0-9A-Z]+([.,~!@#$%&*][a-z0-9A-Z]+)*@([a-z0-9A-Z]+).([a-zA-Z]{2,5})(.[a-zA-Z]{2})*$";
+        public static string Regex_Number = "^91[6-9][0-9]{9}$";
+        public static string Regex_Password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%>,<.&*-_+=])[a-zA-Z0-9!@#$%>,<.&*-_+=]{8,}$";
+        /*
+        public bool ToCheckName(string name)
+        {
+            return Regex.IsMatch(name, Regex_Name);
+        }
+        public bool ToCheckLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, Regex_LastName);
+        }
+       public bool ToCheckMail(string mail)
+        {
+            return Regex.IsMatch(mail, Regex_Email);
+        }
+        public bool ToCheckNumber(string number)
+        {
+            return Regex.IsMatch(number, Regex_Number);
+        }
+        public bool ToCheckPassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+        */
+        public bool ToCheckName(string name) => (Regex.IsMatch(name, Regex_Name));
+
+        public bool ToCheckLastName(string lastName) => (Regex.IsMatch(lastName, Regex_LastName));
+
+        public bool ToCheckMail(string mail) => (Regex.IsMatch(mail, Regex_Email));
+
+        public bool ToCheckNumber(string number) => (Regex.IsMatch(number, Regex_Number));
+
+        public bool ToCheckPassword(string password) => (Regex.IsMatch(password, Regex_Password));
+
+    }
+}
