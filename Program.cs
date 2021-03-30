f6817c82dfae2f4f2bae8ce72c867d09e3890d7d
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-30 08:41:30 +0530

    Anurag Prakash has added

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattens name = new Pattens();
+            Pattens lastname = new Pattens();
+            Pattens email = new Pattens();
+            Pattens number = new Pattens();
+            Pattens password = new Pattens();
+
+            Console.WriteLine("Name To Enter");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Last Name To Enter");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Email To Enter");
+            string Email = Console.ReadLine();
+            Console.WriteLine("Number to Enter using Country Code");
+            string Number = Console.ReadLine();
+            Console.WriteLine("Password To Enter ");
+            Console.WriteLine("1 . Must Have Atleat 8 character" + "\n" + "2 . One Character Should be in upper case" + "\n" + "3 . One character Should Be in Lower Case " + "\n" + "4 . One Special Character " + "\n" + "5. Should Have One Numberic Value");
+            string Password = Console.ReadLine();
+
+
+            if (name.ToCheckName(Name) == true)
+                Console.WriteLine("Valid Input");
+            else
+                Console.WriteLine("InValid Input");
+            if (lastname.ToCheckLastName(LastName) == true)
+                Console.WriteLine("Valid Input");
+            else
+                Console.WriteLine("InValid Input");
+            if (email.ToCheckMail(Email) == true)
+                Console.WriteLine("Valid Input");
+            else
+                Console.WriteLine("InValid Input");
+            if (number.ToCheckNumber(Number) == true)
+                Console.WriteLine("Vaild Input");
+            else
+                Console.WriteLine("Invalid Input");
+            if (password.ToCheckPassword(Password) == true)
+                Console.WriteLine("Vaild Input");
+            else
+                Console.WriteLine("Invalid Input");
+
+
+        }
+      
+    }
+}
